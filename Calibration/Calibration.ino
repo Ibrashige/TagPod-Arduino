a275348308de13658738b22f5ded63af6f4c1350
Ibrashige <61058805+Ibrashige@users.noreply.github.com>
2020-07-03 11:03:47 -0400

    Update Calibration.ino

--- Calibration/Calibration.ino
+++ Calibration/Calibration.ino
@@ -9,7 +9,7 @@
  
 const float offset = 4.1056; // offset for the load cell to indicate 0 load at 2.5 V
 const float vpp = 0.0048828125; // 5V/1024 for ADC conversion
-const float totalLoad = 12.5; // maximum load capacity 12.5 pounds
+const float totalLoad = 22.68; // maximum load capacity 12.5 pounds
 const float a = 1100; // Amplifier Gain
 const float s = 0.0008998; // load cell sensitivity 
 const float ve = 4.97; //Excitation voltage
@@ -36,16 +36,23 @@ void loop() {
 float calculatevoltage(){
   
   points = analogRead(VOUT)- offset; 
-  voltage = points * vpp; 
+  voltage = (points * vpp) ; 
+  Serial.print("calculated voltage is:");
   Serial.println(voltage);
 
   delay(3000);
 }
 
 void calculateweight(){
-  numerator = totalLoad * (voltage-0.55);
+  numerator = totalLoad * (voltage);
   voutmax = s * a * ve;              // raw output voltage to its maximum rated capacity
-  load = numerator/voutmax; // weight in lbs
+  load = numerator/voutmax - 4.18; // weight in kgs
+
+ if(load >= 0 || load <= 0.05 )
+ {
+    load = 0;
+ }
+  Serial.print("calculated weight in kilograms is:");
   Serial.println(load);  
 
   delay(300);
